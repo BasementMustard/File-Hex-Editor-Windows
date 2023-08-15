aabe4a90b4d2b51db7f2feb476a38da0fee85840
basement_mustard <120338237+BasementMustard@users.noreply.github.com>
2023-08-15 17:14:41 -0600

    update statement

---
+++ viruscheck.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+{
+// check for viruses or dangerous malware
+
+cout << "Have you had previous encounters with viruses or dangerous malware in the past with this device? ";
+cin >> viruscheck;
+
+if(viruscheck = true ){
+  cout << "Please send this device to your local repair shop to check for virus/dangerous malware.";
+}
+else{
+  cout << "Alright.";
+}
+}
